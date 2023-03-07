91ad3fc2fba790fcdd578da0977e090b4fbd5c74
7gajaN <ionut.dirvareanu@gmail.com>
2023-03-07 18:05:54 +0200

    simulation works (meesy code)

--- src/Game.java
+++ src/Game.java
@@ -21,20 +21,91 @@ public class Game {
             System.out.println();
         }
     }
-
-    public static int getWinner(ArrayList<Card> cards, int players){
-        int max = 0;
-        int maxIndex = 0;
-
-            for(int i=0;i<players;i++)
-                if( max < cards.get(i).getPower()) {
-                    max = cards.get(i).getPower();
-                            maxIndex = i;
-                }
-            return maxIndex;
+    public static void displayTable(ArrayList<Card> table){
+        for(Card card : table)
+            System.out.print(card.getNumber() + " of " + card.getSign() + " , ");
     }
 
-    static ArrayList<ArrayList<Card>> playerHand = new ArrayList<ArrayList<Card>>();
+    public static ArrayList<Integer> getWinner(ArrayList<Card> cards, ArrayList<Integer> inGame){
+        int max = 0;
+        ArrayList<Card> cardsAux  = new ArrayList<Card>();
+        cardsAux.addAll(cards);
+            for(int i=0;i<inGame.size();i++)
+                if( max < cardsAux.get(i).getPower()) {
+                    max = cardsAux.get(i).getPower();
+                }
+            int j=0;
+            while(j<inGame.size()){
+                if(cardsAux.get(j).getPower() != max){
+                    inGame.remove(j);
+                    cardsAux.remove(j);
+                }
+                else
+                    j++;
+            }
+            return inGame;
+    }
+
+    public static void checkPlayers(ArrayList<ArrayList<Card>> playerHand,int players){
+        for(int i=0; i<= players; i++)
+            if(playerHand.get(i).isEmpty()){
+                playerHand.remove(i);
+                players--;
+                i--;
+            }
+    }
+
+    public static void startWar(ArrayList<ArrayList<Card>> playerHand,int players){
+        ArrayList<Card> table = new ArrayList<Card>();
+        ArrayList<Card> aux = new ArrayList<Card>();
+        ArrayList<Integer> inGame = new ArrayList<Integer>();
+
+        while (playerHand.size()>1){
+            inGame.clear();
+            table.clear();
+            aux.clear();
+            for(int i=0; i<= players; i++)
+                if(playerHand.get(i).isEmpty()){
+                    playerHand.remove(i);
+                    players--;
+                    i--;
+                } //if its an exterior method it doesnt work???
+            displayHands(playerHand,players);
+            for(int i=0;i<players;i++){
+                table.add(playerHand.get(i).get(0));
+                playerHand.get(i).remove(0);
+                inGame.add(i);
+            }
+
+            for(Card card : table){
+                aux.add(card);
+            }
+
+            getWinner(table,inGame);
+            System.out.println(inGame);
+
+            while (inGame.size()>1){
+                System.out.println("@@@@@@@@@@@@@WAR@@@@@@@@@@@@@@@@@@@");
+                for(Card card : table){
+                    aux.add(card);
+                }
+                table.clear();
+                for(Integer i : inGame) {
+                    table.add(playerHand.get(i).get(0));
+                    playerHand.get(i).remove(0);
+                }
+                displayTable(table);
+                System.out.println(inGame);
+                System.out.println(getWinner(table,inGame));
+                aux.addAll(table);
+            }
+            if(inGame.size() == 1){
+                playerHand.get(getWinner(table,inGame).get(0)).addAll(aux);
+            }
+        }
+        }
+
+    public static ArrayList<ArrayList<Card>> playerHand = new ArrayList<ArrayList<Card>>();
     public static void startGame(int players, ArrayList<Card> deck) {
             int playerIndex = 0;
             int cardCounter = 0;
@@ -50,25 +121,26 @@ public class Game {
                 } // have to fix extra index when 52%players != 0
             }
 
-            ArrayList<Card> table = new ArrayList<Card>();
-            while (players > 1){
-                    table.clear();
-                    for(int i=0;i<players;i++){
-                        if(playerHand.get(i).isEmpty()){
-                            playerHand.remove(i);
-                            players = players - 1;
-                            System.out.println("player out");
-                            displayHands(playerHand, players);
 
-                        }
-                    }
-                    displayHands(playerHand, players);
-                    for(int i=0;i<players;i++)
-                    {table.add(playerHand.get(i).get(0));
-                        playerHand.get(i).remove(0);}
-                    System.out.println(getWinner(table,players));
-                    playerHand.get(getWinner(table,players)).addAll(table);
+            startWar(playerHand,players);
+//            while (players > 1){
+//                    table.clear();
+//                    for(int i=0;i<players;i++){
+//                        if(playerHand.get(i).isEmpty()){
+//                            playerHand.remove(i);
+//                            players = players - 1;
+//                            System.out.println("player out");
+//                            displayHands(playerHand, players);
+//
+//                        }
+//                    }
+//                    displayHands(playerHand, players);
+//                    for(int i=0;i<players;i++)
+//                    {table.add(playerHand.get(i).get(0));
+//                        playerHand.get(i).remove(0);}
+//                    System.out.println(getWinner(table,players));
+//                    playerHand.get(getWinner(table,players).get(0)).addAll(table);
             }
 
     }
-}
+
